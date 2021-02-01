2d383324ee5b490f43f4d35700b0119bcb21795c
DevKarenC <49035897+DevKarenC@users.noreply.github.com>
2021-02-01 13:12:53 -0500

    Add basic math operations and operator function (#1)

---
+++ index.js
@@ -0,0 +1,32 @@
+// basic math operators
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+// operate function takes an operator and 2 numbers and calls one of the basic math operator functions above
+function operate(operator, num1, num2) {
+  if (operator === "+") {
+    return add(num1, num2);
+  } else if (operator === "-") {
+    return subtract(num1, num2);
+  } else if (operator === "*") {
+    return multiply(num1, num2);
+  } else if (operator === "/") {
+    return divide(num1, num2);
+  }
+}
+
+console.log(operate("/", 10, 2));
