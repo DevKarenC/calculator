d4f44b07243aa04bd4beff7ea21b953bd76474de
DevKarenC <49035897+DevKarenC@users.noreply.github.com>
2021-02-01 15:48:51 -0500

    Add display functionality to calculator (#3)

--- index.js
+++ index.js
@@ -29,4 +29,40 @@ function operate(operator, num1, num2) {
   }
 }
 
-console.log(operate("/", 10, 2));
+// console.log(operate("/", 10, 2));
+
+// display numbers on the calculator
+function display() {
+  let decimal = 0;
+  let displayScreen = document.querySelector(".display");
+  let displayValue = 0;
+  const numbers = Array.from(document.querySelectorAll(".numbers")).map(
+    (number) =>
+      number.addEventListener("click", () => {
+        // edge case 1: numbers cannot start with a zero
+        if (displayScreen.textContent === "" && number.textContent === "0") {
+          return;
+        }
+        // edge case 2: if decimal point is clicked before other numbers, 0 is appended
+        if (displayScreen.textContent === "" && number.textContent === ".") {
+          displayScreen.textContent += "0.";
+          decimal++;
+        }
+        // edge case 3: only one decimal point per number
+        if (decimal === 0 && number.textContent === ".") {
+          displayScreen.textContent += number.textContent;
+          decimal++;
+        }
+        if (decimal >= 1 && number.textContent === ".") {
+          return;
+        }
+        // console.log(number.textContent);
+        displayScreen.textContent += number.textContent;
+        console.log(displayScreen.textContent);
+        displayValue = parseInt(displayScreen.textContent);
+        // console.log(displayValue);
+      })
+  );
+}
+
+display();
