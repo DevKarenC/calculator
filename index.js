c4f176302f68dfe1f9cc2c7e12b87edff1d3f5f7
DevKarenC <49035897+DevKarenC@users.noreply.github.com>
2021-02-02 14:14:58 -0500

    Handle mousedown and mouseup events with brightness styling (#5)

--- index.js
+++ index.js
@@ -101,6 +101,16 @@ function handleKeyPad() {
   }
 }
 
-document
-  .querySelectorAll(".calculator-item")
-  .forEach((item) => item.addEventListener("click", handleKeyPad));
+function addTransition() {
+  this.classList.add("calculator-item-onclick");
+}
+
+function removeTransition() {
+  this.classList.remove("calculator-item-onclick");
+}
+
+document.querySelectorAll(".calculator-item").forEach((item) => {
+  item.addEventListener("click", handleKeyPad);
+  item.addEventListener("mousedown", addTransition);
+  item.addEventListener("mouseup", removeTransition);
+});
