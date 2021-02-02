4ba366c26187528d283c20276760050fd72ad79c
DevKarenC <49035897+DevKarenC@users.noreply.github.com>
2021-02-02 13:53:26 -0500

    Separate display function and handle keypad inputs (#4)

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // basic math operators
-
 function add(num1, num2) {
   return num1 + num2;
 }
@@ -22,47 +21,86 @@ function operate(operator, num1, num2) {
     return add(num1, num2);
   } else if (operator === "-") {
     return subtract(num1, num2);
-  } else if (operator === "*") {
+  } else if (operator === "×") {
     return multiply(num1, num2);
-  } else if (operator === "/") {
+  } else if (operator === "÷") {
     return divide(num1, num2);
   }
 }
 
-// console.log(operate("/", 10, 2));
+let display = document.querySelector(".display");
+let decimal = 0;
+let operator = "";
+let num1 = "";
+let num2 = "";
+let solution = "";
 
 // display numbers on the calculator
-function display() {
-  let decimal = 0;
-  let displayScreen = document.querySelector(".display");
-  let displayValue = 0;
-  const numbers = Array.from(document.querySelectorAll(".numbers")).map(
-    (number) =>
-      number.addEventListener("click", () => {
-        // edge case 1: numbers cannot start with a zero
-        if (displayScreen.textContent === "" && number.textContent === "0") {
-          return;
-        }
-        // edge case 2: if decimal point is clicked before other numbers, 0 is appended
-        if (displayScreen.textContent === "" && number.textContent === ".") {
-          displayScreen.textContent += "0.";
-          decimal++;
-        }
-        // edge case 3: only one decimal point per number
-        if (decimal === 0 && number.textContent === ".") {
-          displayScreen.textContent += number.textContent;
-          decimal++;
-        }
-        if (decimal >= 1 && number.textContent === ".") {
-          return;
-        }
-        // console.log(number.textContent);
-        displayScreen.textContent += number.textContent;
-        console.log(displayScreen.textContent);
-        displayValue = parseInt(displayScreen.textContent);
-        // console.log(displayValue);
-      })
-  );
+function displayNumber(value) {
+  display.textContent += value;
 }
 
-display();
+// add event listeners to the calculator buttons
+function handleKeyPad() {
+  // edge case 1: num1 cannot start with a zero
+  if (display.textContent === "" && this.textContent === "0") {
+    return;
+  }
+  // edge case 2: if decimal point is clicked before other numbers, 0 is appended
+  if (display.textContent === "" && this.textContent === ".") {
+    decimal++;
+    displayNumber("0.");
+  }
+  // edge case 3: only one decimal point per number
+  if (decimal === 0 && this.textContent === ".") {
+    decimal++;
+    displayNumber(this.textContent);
+  }
+  if (decimal >= 1 && this.textContent === ".") {
+    return;
+  }
+  // handle number inputs
+  if (this.classList.contains("numbers")) {
+    displayNumber(this.textContent);
+  }
+  // handle operator inputs
+  if (this.textContent === "+") {
+    operator = "+";
+    num1 = display.textContent;
+    console.log("This is num1: " + num1);
+    display.textContent = "";
+    decimal = 0;
+  }
+  if (this.textContent === "-") {
+    operator = "-";
+    num1 = display.textContent;
+    console.log("This is num1: " + num1);
+    display.textContent = "";
+    decimal = 0;
+  }
+  if (this.textContent === "×") {
+    operator = "×";
+    num1 = display.textContent;
+    console.log("This is num1: " + num1);
+    display.textContent = "";
+    decimal = 0;
+  }
+  if (this.textContent === "÷") {
+    operator = "÷";
+    num1 = display.textContent;
+    console.log("This is num1: " + num1);
+    display.textContent = "";
+    decimal = 0;
+  }
+  // handle evaluate input
+  if (this.textContent === "=") {
+    num2 = display.textContent;
+    console.log("This is num2: " + num2);
+    display.textContent = "";
+    displayNumber(operate(operator, parseInt(num1), parseInt(num2)));
+  }
+}
+
+document
+  .querySelectorAll(".calculator-item")
+  .forEach((item) => item.addEventListener("click", handleKeyPad));
